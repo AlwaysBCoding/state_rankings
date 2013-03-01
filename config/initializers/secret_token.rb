27e51af6fab3a42e5318a1d35c30ec75abbc6e38
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-02-28 23:02:15 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StateRankings::Application.config.secret_token = '5b5ae13a4c9512422e05055f2fa052b4f366045572b773609d78d2e032723aec373beea7de847bf7428a026a1b7bfa0c078e021cfc59899b6081390411099cc3'
